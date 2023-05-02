d9a29f06eab6162190a669d8fbdfe6e106ae59ad
Rishabh <rishu88042@gmail.com>
2023-05-02 16:51:40 +0530

    connected to server, mongodb and password hashing is complete

---
+++ controllers/authController.js
@@ -0,0 +1,57 @@
+import userModel from "../models/userModel.js";
+import { hashPassword } from "./../helpers/authHelper.js";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ error: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ error: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ error: "Phone is Required" });
+    }
+    if (!address) {
+      return res.send({ error: "Adress is Required" });
+    }
+
+    // check user
+    const existingUser = await userModel.findOne({ email });
+    //existing user
+    if (existingUser) {
+      return res.status(200).send({
+        success: false, //in videos it was 'true'
+        message: "Already Registered please login",
+      });
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+};
